39f07f5903da9fb8411375bf2e998bcf09d148cc
Deniss Ignatjevs <den4ik220705@gmail.com>
2020-04-04 12:07:36 +0300

    Version 12:07

---
+++ MyApp.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GitHubWorking
+{
+    public partial class MyApp : Form
+    {
+        public MyApp()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("I am learning GitHub");
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
